48802784fb1290e1d1cddae8a5c0f4768f4d857e
Tal Charnes <tal32123@gmail.com>
2023-11-27 00:13:56 +0200

    fix: lint in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-
-export default function App({ Component, pageProps }) {
+import type { AppProps } from 'next/app';
+export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
   }
