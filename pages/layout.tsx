f6e257f41b61304fa401b409693b66a89f57da45
Tal Charnes <tal32123@gmail.com>
2023-11-27 00:21:11 +0200

    fix: build

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Pentzi',
-  description: 'Next generation finance',
-}
+
 export default function RootLayout({
   children,
 }: {
