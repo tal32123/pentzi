1a0e4f4adebdd7f3451d19ce963e5b2d3dcc96ea
Tal Charnes <tal32123@gmail.com>
2023-11-27 21:48:56 +0200

    style: dashboard card style

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,35 +1,34 @@
-import LineChart from "@/components/charts/line-chart"
-import PentziCard from "@/components/pentzi-card"
-import Sidebar from "@/components/sidebar"
-import { Grid } from "@mui/material"
-import UtilStyles from './../../styles/utils.module.css'
+import LineChart from "@/components/charts/line-chart";
+import PentziCard from "@/components/pentzi-card";
+import Sidebar from "@/components/sidebar";
+import { Grid } from "@mui/material";
+import UtilStyles from "./../../styles/utils.module.css";
 const Dashboard = () => {
-
   return (
     <div>
       <Sidebar></Sidebar>
       <div className={UtilStyles.center}>
-      <h1 className={UtilStyles.lightText}>your data</h1>
+        <h1 className={UtilStyles.lightText}>your data</h1>
       </div>
       <Grid container spacing={2}>
-      {[...Array(4)].map((_, index) => ( 
-        <Grid item xs={12} sm={6} md={6} key={index}>
-                <div className={UtilStyles.center} >
-
-          <PentziCard>
-            <LineChart stockData={ [
-              { date: '2023-01-01', price: Math.random() * 100 },
-              { date: '2023-01-02', price: Math.random() * 200  },
-              { date: '2023-01-03', price: Math.random() * 500  },
-            ]}/>
-            </PentziCard>
+        {[...Array(4)].map((_, index) => (
+          <Grid item xs={12} sm={6} md={6} key={index}>
+            <div className={UtilStyles.center}>
+              <PentziCard>
+                <LineChart
+                  stockData={[
+                    { date: "2023-01-01", price: Math.random() * 100 },
+                    { date: "2023-01-02", price: Math.random() * 200 },
+                    { date: "2023-01-03", price: Math.random() * 500 },
+                  ]}
+                />
+              </PentziCard>
             </div>
-
-        </Grid>
-      ))}
+          </Grid>
+        ))}
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
