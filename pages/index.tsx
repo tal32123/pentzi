988b7f4f43492af498b0c83e8c014b2cdc5d7d20
Tal Charnes <tal32123@gmail.com>
2023-11-27 20:47:06 +0200

    test: unused var

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,7 @@ import LoginForm from "@/components/login-form";
 export default function Home() {
   const pentziFooterText =
     "Pentzi is a revolutionary pension investing app designed to simplify and enhance the way you manage your retirement funds. Pentzi makes it easy for anyone to take control of their pension investments. The app offers a range of investment options, tailored to meet individual retirement goals and risk preferences. Whether you're a seasoned investor or new to the world of pensions, Pentzi provides all the tools and resources needed to grow your retirement savings effectively. Stay informed with real-time market insights, easy portfolio management, and personalized investment strategies, all at your fingertips. Start securing your financial future today with Pentzi, the smart way to invest in your pension.";
-  console.log("here");
-
+  const unusedVar = "";
   return (
     <main className={styles.main}>
       <div className={styles.center}>
