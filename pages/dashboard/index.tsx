256422b5ee8ef3da9fb39e44635d867a7a57eaaf
Tal Charnes <tal32123@gmail.com>
2023-11-27 21:42:02 +0200

    style: center and fix title in dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,22 +2,24 @@ import LineChart from "@/components/charts/line-chart"
 import PentziCard from "@/components/pentzi-card"
 import Sidebar from "@/components/sidebar"
 import { Grid } from "@mui/material"
-
+import UtilStyles from './../../styles/utils.module.css'
 const Dashboard = () => {
 
   return (
     <div>
       <Sidebar></Sidebar>
-      <h1>hello</h1>
+      <div className={UtilStyles.center}>
+      <h1 className={UtilStyles.lightText}>your data</h1>
+      </div>
       <Grid container spacing={2}>
       {[...Array(4)].map((_, index) => ( 
         <Grid item xs={12} sm={6} md={3} key={index}>
           <PentziCard>
             <LineChart stockData={ [
-    { date: '2023-01-01', price: Math.random() * 100 },
-    { date: '2023-01-02', price: Math.random() * 200  },
-    { date: '2023-01-03', price: Math.random() * 500  },
-  ]}/>
+              { date: '2023-01-01', price: Math.random() * 100 },
+              { date: '2023-01-02', price: Math.random() * 200  },
+              { date: '2023-01-03', price: Math.random() * 500  },
+            ]}/>
             </PentziCard>
         </Grid>
       ))}
